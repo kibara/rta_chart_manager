a26edd00d3851dbe2144388375b4e8bb52d0540d
Sakakibara <skw.sakakibara@gmail.com>
2025-01-19 19:40:58 +0900

    チャート詳細のカルーセル化

--- lib/screen/chart_details.dart
+++ lib/screen/chart_details.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:rta_chart_manager/database/collections.dart';
 import 'package:rta_chart_manager/database/kvs_utils.dart';
+import 'package:rta_chart_manager/database/models/action_item_model.dart';
 import 'package:rta_chart_manager/database/models/chart_detail_model.dart';
 import 'package:rta_chart_manager/database/models/chart_summary_model.dart';
 
@@ -16,35 +17,84 @@ class ChartDetails extends StatefulWidget {
 }
 
 class _ChartDetailsState extends State<ChartDetails> {
-  // ステートで保持する値
-  // {
-  //     "chart_title": [{ * chart_part1_json * }, { * chart_part2_json * }],
-  //     ...
-  // }
+  late final Box<ChartSummaryModel> _chartSummaryBox;
   late final Box<ChartDetailModel> _chartDetailBox;
+  late final List<ChartSummaryModel> _chartSummaryList;
+  late final List<ChartDetailModel> _detailList;
+  late final String _chartId;
+  int currentPage = 0;
 
   @override
   void initState() {
+    // chartId
+    _chartId = widget.chartSummary.chartId;
+
+    // currentPage
+    currentPage = widget.chartSummary.orderIndex;
+
+    // chartSummary
+    _chartSummaryBox =
+        KvsUtils.getBox<ChartSummaryModel>(Collections.chartSummary);
+    _chartSummaryList =
+        List.from(_chartSummaryBox.values.where((s) => s.chartId == _chartId));
+
+    // chartDetail
     _chartDetailBox =
         KvsUtils.getBox<ChartDetailModel>(Collections.chartDetails);
+    _detailList =
+        List.from(_chartDetailBox.values.where((d) => d.chartId == _chartId));
+
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    PageController pageController = PageController(initialPage: currentPage);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.chartSummary.title),
       ),
-      body: ValueListenableBuilder(
-        valueListenable: _chartDetailBox.listenable(),
-        builder: (context, box, widget) {
-          return Center(
-            child: Text("aaa"),
-          );
+      body: Center(
+          child: PageView.builder(
+        controller: pageController,
+        itemCount: _detailList.length,
+        itemBuilder: (context, index) {
+          ChartDetailModel detailModel =
+              _detailList.where((d) => d.orderIndex == index).first;
+
+          return ListView.builder(
+              itemCount: detailModel.actionItems.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Icon(Icons.person_pin_circle),
+                  title: Text(detailModel!.actionItems[index].text),
+                );
+              });
         },
-      ),
+      )),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        ActionItemModel actionItem = ActionItemModel('み', 1, 0);
+
+        setState(() {
+          _detailList[currentPage].actionItems.add(actionItem);
+          _detailList[currentPage].save();
+        });
+      }),
     );
+  }
+}
+
+class _DetailPage extends StatelessWidget {
+  final List<ActionItemModel> actionItems;
+
+  const _DetailPage({
+    required this.actionItems,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('abc');
   }
 }
