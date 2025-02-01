ab5fb36e030117742b8b847ca8006b0574b7d70e
Sakakibara <skw.sakakibara@gmail.com>
2025-02-01 22:05:06 +0900

    duration extension を修正

--- lib/component/extension/duration_extension.dart
+++ lib/component/extension/duration_extension.dart
@@ -1,22 +1,18 @@
 extension DurationExtension on Duration {
   String fmtHHmmssSS() {
-    return _formatDuration(this);
-  }
-
-  String _formatDuration(Duration d) {
     String twoDigits(int n) {
       if (n.isNaN) return "00";
       return n.toString().padLeft(2, "0").substring(0, 2);
     }
 
     // 符号の取得
-    String negativeSign = d.isNegative ? '-' : '';
+    String negativeSign = isNegative ? '-' : '';
 
     // 各値の絶対値を取得
-    int hour = d.inHours.abs();
-    int min = d.inMinutes.remainder(60).abs();
-    int sec = d.inSeconds.remainder(60).abs();
-    int mills = d.inMilliseconds.remainder(1000).abs();
+    int hour = inHours.abs();
+    int min = inMinutes.remainder(60).abs();
+    int sec = inSeconds.remainder(60).abs();
+    int mills = inMilliseconds.remainder(1000).abs();
 
     // 各値を2桁の文字列に変換
     String strHour = twoDigits(hour);
