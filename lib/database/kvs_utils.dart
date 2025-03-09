2775363c1c951e5a461e8681ce2943647b5bb42d
Sakakibara <skw.sakakibara@gmail.com>
2025-03-09 21:34:56 +0900

    KVSUtilsにコメントを入れておく

--- lib/database/kvs_utils.dart
+++ lib/database/kvs_utils.dart
@@ -1,22 +1,30 @@
 import 'package:hive_flutter/hive_flutter.dart';
 
 class KvsUtils {
+  /// Hiveの初期化
   static Future<void> init() async {
     await Hive.initFlutter();
   }
 
+  /// Hiveを閉じる
   static Future<void> close() async {
     await Hive.close();
   }
 
+  /// Boxを開く
+  /// アプリを開くタイミングで先に開いておく必要がある
   static Future<Box> openBox<T>(String boxName) {
     return Hive.openBox<T>(boxName);
   }
 
+  /// Boxを取得する
+  /// 事前にopenBoxしておく必要がある
   static getBox<T>(String boxName) {
     return Hive.box<T>(boxName);
   }
 
+  /// HiveAdapterを登録する
+  /// openBoxの前に登録しておく必要がある
   static registerAdapter<T>(TypeAdapter<T> adapter) {
     Hive.registerAdapter<T>(adapter);
   }
